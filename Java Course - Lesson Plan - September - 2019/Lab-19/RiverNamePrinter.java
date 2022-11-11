afb73cb0a43f6d52024d21fe9d3e10430652d410
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-11-12 03:27:00 +0530

    Create RiverNamePrinter.java

---
+++ Java Course - Lesson Plan - September - 2019/Lab-19/RiverNamePrinter.java
@@ -0,0 +1,9 @@
+package q10936;
+public class RiverNamePrinter {
+	public static void main(String[] args) {
+		String[] namesArr = { "Ganga", "Yamuna", "Godavari", "Krishna", "Narmada", "Kaveri" };
+		for (String river : namesArr) {
+			System.out.println(river);
+		}
+	}
+}
