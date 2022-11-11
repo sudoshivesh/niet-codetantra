1b456f8d043f3df7e8096180e43f8b9d61050c2d
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-11-12 03:27:33 +0530

    Create MountainNamePrinter.java

---
+++ Java Course - Lesson Plan - September - 2019/Lab-19/MountainNamePrinter.java
@@ -0,0 +1,9 @@
+package q10937;
+public class MountainNamePrinter{
+	public static void main(String[] args){
+		String[] arr={"Nanda Devi","Kamet","K12","Dunagiri"};
+		for(String mountain : arr){
+			System.out.println(mountain);
+		}
+	}
+}
