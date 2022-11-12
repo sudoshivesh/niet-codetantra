ae8f993b65d82718084fb46ef19314a63779cbfd
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-11-12 11:57:38 +0530

    Update ElementCheck3.java

--- Java Course - Lesson Plan - September - 2019/Lab-21/ElementCheck3.java
+++ Java Course - Lesson Plan - September - 2019/Lab-21/ElementCheck3.java
@@ -1,1 +1,16 @@
+package q11055;
 
+public class ElementCheck {
+	
+/**
+ * Find first and last elements of the array are same are not
+ * 
+ * @return true if both are same else return false
+ */
+	public boolean elementFinder(int[] arr) {
+		if(arr[0]==arr[arr.length-1])
+		return true;
+		else
+		return false;
+	}
+}
