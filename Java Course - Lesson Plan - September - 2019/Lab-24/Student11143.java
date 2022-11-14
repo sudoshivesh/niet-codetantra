9e413a62c430c80aca74883a5017def68752ac14
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-11-14 10:27:25 +0530

    Create Student11143.java

---
+++ Java Course - Lesson Plan - September - 2019/Lab-24/Student11143.java
@@ -0,0 +1,13 @@
+package q11143;
+public class Student {
+	private String id;
+	private String name;
+	private int age;
+	private char gender;
+	public Student(String id, String name, int age, char gender) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+}
