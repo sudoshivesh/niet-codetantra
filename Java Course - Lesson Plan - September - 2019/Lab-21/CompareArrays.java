b3161cfe256a4b6c750006f6be680948dc9e44cd
Shivesh Shivam <78317220+sudoshivesh@users.noreply.github.com>
2022-11-14 09:57:04 +0530

    Create CompareArrays.java

---
+++ Java Course - Lesson Plan - September - 2019/Lab-21/CompareArrays.java
@@ -0,0 +1,19 @@
+package q11066;
+
+public class CompareArrays {
+	/**
+	 * Find the length of the two arrays are equal or not
+	 * 
+	 * 
+	 * @return result
+	 */ 
+	
+	
+	
+	public boolean compareArrays(int[] arr1, int[] arr2) {
+		if(arr1.length==arr2.length)
+		return true;
+		else
+		return false;
+	}
+}
