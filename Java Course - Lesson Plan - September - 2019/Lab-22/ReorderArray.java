93743d1ac6139e36335f4a2a18688c9b0994cd25
Mahek Agarwal <118847439+mahekagar29@users.noreply.github.com>
2024-02-24 14:55:43 +0530

    Update ReorderArray.java
    This is a code for ReorderArray.

--- Java Course - Lesson Plan - September - 2019/Lab-22/ReorderArray.java
+++ Java Course - Lesson Plan - September - 2019/Lab-22/ReorderArray.java
@@ -1,1 +1,45 @@
+package q11089;
 
+public class ReorderArray {
+	/**
+	 *Arrange all the zeros should come infront of the array
+	 * 
+	 * 
+	 * 
+	 *@return array
+	 * 
+	 */
+	 
+	public int[] reorder(int[] arr) {
+		//Write your code here
+		int[] a = new int[arr.length];
+		
+		int j=0;
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			if(arr[i] ==0){
+				
+				a[j]=0;
+				
+				j++;
+				
+			}
+		}
+			
+			for (int t=0; t<arr.length; t++){
+				
+				if(arr[t] !=0){
+					
+					a[j] = arr[t];
+					
+					j++;
+					
+				}
+				
+			}
+			
+			return a;
+		
+	}
+}
